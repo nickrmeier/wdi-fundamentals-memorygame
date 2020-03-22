3be25574901b0918411a566139eaa2a13cf7338a
Nicholas Meier <nicholasmeier@Nicholass-MacBook-Pro.local>
2020-03-22 12:34:21 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,1 +1,20 @@
-console.log("Up and running!");
+
+const cards = ["queen", "queen", "king", "king"];
+
+const cardsInPlay = [];
+cardsInPlay.push("queen");
+cardsInPlay.push("king");
+
+const cardOne = cards[0];
+const cardTwo = cards[2];
+
+console.log("User flipped " + cardOne); 
+console.log("User flipped " + cardTwo); 
+
+if (cardsInPlay.length === 2);
+	else if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+}
+	else if (cardsInPlay[0] != cardsInPlay[1]) {
+	alert("Sorry,try again.");
+}
